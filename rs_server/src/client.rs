1c91698df1d9c6e5618a274d1aa6554f6804b594
Begun0vM <maksimilianbegunov@inbox.ru>
2026-01-23 12:52:34 +0300

    client: move CLI to options()

--- rs_server/src/client.rs
+++ rs_server/src/client.rs
@@ -17,26 +17,88 @@ async fn download_file() -> Result<(), String> {
     Ok(())
 }
 
-async fn _send_message() -> Result<(), String> {
-    // smth
+async fn _send_message(url: &String) -> Result<(), String> {
+    println!("[>] Input name: ");
+    let mut from: String = String::new();
+    io::stdin().read_line(&mut from).expect("[!] Cannot read name");
+    
+    println!("[>] Input message: ");
+    let mut text: String = String::new();
+    io::stdin().read_line(&mut text).expect("[!] Cannot read message");
+    
+    let message: Message = Message {
+        text: text.trim().to_string(),
+        from: from.trim().to_string(),
+    };
+    
+    let client: reqwest::Client = reqwest::Client::new();
+    match client.post(&format!("{}/send", url))
+        .json(&message) 
+        .send()
+        .await {
+        Ok(response) => {
+            if response.status().is_success() {
+                let response_msg: Message = response.json().await.expect("");
+                println!("[=] Server response: {}", response_msg.text);
+            } else {
+                println!("[!] {}", response.status());
+            }
+        }
+        Err(e) => println!("[!] {e}"),
+    }
     
     Ok(())
 }
 
-async fn options() -> Result<(), String>{
+async fn check_connection(url: &String) {
+    match reqwest::get(&*url).await {
+        Ok(response) => {
+            if response.status().is_success() {
+                let text: String = response.text().await.unwrap_or_default();
+                println!("[=] Connnect successful! Response: {}", text);
+            } else {
+                println!("[!] {}", response.status());
+            }
+        }
+        Err(e) => println!("[!] {e}"),
+    }
+}
+
+async fn get_all_messages(url: &String) {
+    match reqwest::get(&format!("{}/messages", url)).await {
+        Ok(response) => {
+            if response.status().is_success() {
+                let messages: Vec<String> = response.json().await.expect("");
+                println!("\n[=] All messages:");
+                for msg in messages {
+                    println!("\t-{}", msg);
+                }
+            } else {
+                println!("[!] {}", response.status());
+            }
+        }
+        Err(e) => println!("[!] {e}"),
+    }
+}
+
+async fn options(url: &String) {
     // Client CLI
     loop {
         println!("[0] Exit");
         println!("[1] Download file");
         println!("[2] Send message");
+        println!("[3] Get all messages");
+        println!("[4] Check connection");
         
         let mut choice: String = String::new();
         io::stdin().read_line(&mut choice).expect("[!] Cannot read choice");
         
         match choice.trim() {
-            "0" => (),
-            "1" => download_file().await?,
-            "2" => _send_message().await?,
+            "0" => break,
+            "1" => download_file().await.expect("[!] Err with download file"),
+            "2" => _send_message(url).await.expect("[!] Err with sending message"),
+            "3" => get_all_messages(url).await,
+            "4" => check_connection(url).await,
             _ => println!("[!] Unknown command"),
         }
     }
@@ -44,80 +106,5 @@ async fn options() -> Result<(), String>{
 
 pub async fn connect_as_guest() {
     let base_url: String = connect_to_network().await;
-
-    // options().await;
-    
-    loop {
-        println!("[0] Exit");
-        println!("[1] Check connection");
-        println!("[2] Send message");
-        println!("[3] Get all messages");
-        
-        let mut choice = String::new();
-        io::stdin().read_line(&mut choice).expect("[!] Cannot read choose");
-        
-        match choice.trim() {
-            "1" => {
-                match reqwest::get(&base_url).await {
-                    Ok(response) => {
-                        if response.status().is_success() {
-                            let text: String = response.text().await.unwrap_or_default();
-                            println!("[+] Connnect successful! Response: {}", text);
-                        } else {
-                            println!("[!] {}", response.status());
-                        }
-                    }
-                    Err(e) => println!("[!] {e}"),
-                }
-            }
-            "2" => {
-                println!("[>] Input name: ");
-                let mut from: String = String::new();
-                io::stdin().read_line(&mut from).expect("[!] Cannot read name");
-                
-                println!("[>] Input message: ");
-                let mut text: String = String::new();
-                io::stdin().read_line(&mut text).expect("[!] Cannot read message");
-                
-                let message: Message = Message {
-                    text: text.trim().to_string(),
-                    from: from.trim().to_string(),
-                };
-                
-                let client: reqwest::Client = reqwest::Client::new();
-                match client.post(&format!("{}/send", base_url))
-                    .json(&message) 
-                    .send()
-                    .await {
-                    Ok(response) => {
-                        if response.status().is_success() {
-                            let response_msg: Message = response.json().await.expect("");
-                            println!("[=] Server response: {}", response_msg.text);
-                        } else {
-                            println!("[!] {}", response.status());
-                        }
-                    }
-                    Err(e) => println!("[!] {e}"),
-                }
-            }
-            "3" => {
-                match reqwest::get(&format!("{}/messages", base_url)).await {
-                    Ok(response) => {
-                        if response.status().is_success() {
-                            let messages: Vec<String> = response.json().await.expect("");
-                            println!("\n[=] All messages:");
-                            for msg in messages {
-                                println!("\t-{}", msg);
-                            }
-                        } else {
-                            println!("[!] {}", response.status());
-                        }
-                    }
-                    Err(e) => println!("[!] {e}"),
-                }
-            }
-            "0" => break,
-            _ => println!("[!] Unknown command"),
-        }
-    }
+    options(&base_url).await;
 }
